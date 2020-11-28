4bce672392f6d919265fef4defc91a132a68aa72
higorasilverio <higorasilverio@outlook.com>
2020-11-28 15:14:14 -0300

    Second commit: IndexDB

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+import {
+  set as setItem,
+  get as getItem,
+  keys as getKeys,
+} from "https://cdn.jsdelivr.net/npm/idb-keyval@3/dist/idb-keyval.mjs";
+
 class App {
   constructor() {
     console.log("Initialized");
@@ -16,19 +22,23 @@ class App {
     const keyInput = document.getElementById("key");
     const valueInput = document.getElementById("value");
     if (keyInput.value && valueInput.value) {
-      localStorage.setItem(keyInput.value, valueInput.value);
-      this.listStorageValues();
+      setItem(keyInput.value, valueInput.value)
+        .then(() => {
+            this.listStorageValues();
+        });
     }
   }
 
-  listStorageValues() {
+  async listStorageValues() {
     const storageValues = document.getElementById("storageValues");
-    const toHtml = (key) => {
-      const value = localStorage.getItem(key);
+    const toHtml = async (key) => {
+      const value = await getItem(key);
       return `<p>${key}: ${value}</p>`;
     };
-    const htmlOutput = Object.keys(localStorage).sort().map(toHtml).join("");
-    storageValues.innerHTML = htmlOutput;
+    // const htmlOutput = Object.keys(localStorage).sort().map(toHtml).join("");
+    const keys = await getKeys();
+    const htmlOutput = await Promise.all(keys.map(toHtml));
+    storageValues.innerHTML = htmlOutput.sort().join("");
   }
 }
 
